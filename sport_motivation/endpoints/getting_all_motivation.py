732914d51cd7b9d2793c83c9b938825da9ba79df
MaxWorkIT <rumyantsev1mos1@gmail.com>
2024-11-15 17:45:12 +0300

    Был удалён эндпоинт + сопутствующие файлы/строки по удалению одной записи из бд, добавлена валидация к эндпоинту + сопутствующим файлам/строкам по получению одной записи из бд
    Signed-off-by: Макс <rumyantsev1m0s1@gmail.com>

--- sport_motivation/endpoints/getting_all_motivation.py
+++ sport_motivation/endpoints/getting_all_motivation.py
@@ -2,5 +2,4 @@ from main_files.motivation_fake_db import sport_mtv
 
 
 def endpoint_getting_all_motivation() -> list | str:
-    return sport_mtv if sport_mtv \
-        else f"no data (sport_mtv = {sport_mtv})"
+    return sport_mtv if sport_mtv else f"there is no data in the database"
